62b894cc9ea35212d458fef475e1783bfe85b29b
Diego Pereira <82544146+DiegoPereira12@users.noreply.github.com>
2022-04-25 19:51:15 -0300

    Delete remover.py

--- remover.py
+++
@@ -1,6 +0,0 @@
-def remove_item(lista_inquilinos,*args):
-        deletar = list(args)
-        for item in deletar:
-            while item in lista_inquilinos:
-                lista_inquilinos.clear(item)
-            return lista_inquilinos
